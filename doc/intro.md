2e4900c8a0de244b54fb6fdcf63f30f983b3e997
kevin.li <li_zhaoyu@msn.com>
2018-02-22 09:05:59 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sit-stand
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sit_stand/core_test.clj
@@ -0,0 +1,7 @@
+(ns sit-stand.core-test
+  (:require [clojure.test :refer :all]
+            [sit-stand.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
