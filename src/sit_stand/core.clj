53b57a46a004c75ebb78c16c19827cb1307e742e
Kevin.Li <li_zhaoyu@msn.com>
2018-02-24 20:57:41 +0800

    代码组织优化

--- src/sit_stand/core.clj
+++ src/sit_stand/core.clj
@@ -19,50 +19,38 @@
     {:name "api_secret" :content "cnMO24ie4I41m45ns8Y0d5_JabrWHfXs"}
     {:name "image_file" :content (clojure.java.io/file file-name)}]})
 
-(defn sit? [rectangle]
-  (let [height (get rectangle "height")
+(defn rect->posture [body-dic]
+  (let [rectangle (get body-dic "humanbody_rectangle")
+        height (get rectangle "height")
         width (get rectangle "width")]
-    (if (< 1/3 (/ width height))
-      true
-      false)))
-
-(defn human? [respond-body]
-  (let [humanbodies (get respond-body "humanbodies")
-        body-count (count humanbodies)]
-    (if (= 0 body-count)
-      false
-      true)))
-
-(defn sit-or-stand [human-body]
-  (if (sit? (human-body "humanbody_rectangle"))
-    "坐着"
-    "站着")
-  )
-
-(defn validate-args
-  [args]
-  (let [{:keys [options  errors summary]} (parse-opts args cli-options)]
     (cond
-      (:help options) ; help => exit OK with usage summary
-      {:summary summary :ok? true}
-      errors  {:exit-message (error-msg errors)}
-      (and (= 1 (count arguments))
-           (#{"start" "stop" "status"} (first arguments)))
-      :else ; failed custom validation => exit with usage summary
-      {:exit-message (usage summary)})))
+      (>= 1/3 (/ width height)) :stand
+      (<  1   (/ width height)) :lay
+      :else                     :sit
+      )))
+
+(defn respond-body [respond]
+  (let [status (:status respond)]
+    (if (= 200 status)
+      (:body respond)
+      (println "something went wrong with the server"))))
+
+(defn respond-content->body-array [respond-body]
+  (let [respond-dic (json/read-str respond-body)
+        body-array (get respond-dic "humanbodies") ]
+    (if (zero? (count body-array))
+      (println "no people detected")
+      body-array)))
+
 
 (defn make-request [file-name]
   (let [respond (client/post url (multi-part file-name))
         status (:status respond)]
-    (pprint/pprint respond)
-    (if (= 200 status)
-      (let [respond-body (json/read-str (get respond :body))]
-        (if (human? respond-body)
-          (map sit-or-stand
-               (respond-body "humanbodies" ))
-          (println "没有检测到人脸，或者图片格式错误")))
-      )))
-
+    (pprint respond)
+    (->> respond
+         (respond-body)
+         (respond-content->body-array)
+         (map rect->posture ))))
 
 (defn -main [& args]
   (let [options (parse-opts args cli-options)
